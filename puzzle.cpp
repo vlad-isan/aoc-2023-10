c06c2fd2afcaaf1f3122b719eeb7d5223b85ac11
Vlad Isan <vlad@innerbyte.uk>
2023-12-10 16:53:31 +0000

    part 1

--- puzzle.cpp
+++ puzzle.cpp
@@ -71,11 +71,26 @@ int puzzle_2(const std::string &base_file_path) {
 int do_puzzle_1(std::ifstream &file) {
     std::string line;
 
+    Grid grid;
+    std::pair<int, int> start;
+
     while (std::getline(file, line)) {
-        fmt::println("{}", line);
+        GridRow row;
+
+        for (auto c: line) {
+            row.emplace_back(direction_map.at(c));
+
+            if (direction_map.at(c) == Direction::START) {
+                start = {grid.size(), row.size() - 1};
+            }
+        }
+
+        grid.emplace_back(row);
     }
 
-    return 0;
+    calculate_start_directions(grid, start);
+
+    return get_steps_to_farthest_tile(grid, start);
 }
 
 int do_puzzle_2(std::ifstream &file) {
@@ -86,4 +101,91 @@ int do_puzzle_2(std::ifstream &file) {
     }
 
     return 0;
+}
+
+void calculate_start_directions(Grid &grid, const std::pair<int, int> &start) {
+    auto [row, col] = start;
+
+    auto start_direction = grid[row][col];
+    Direction dir = Direction::BLOCKED;
+
+    if (row > 0 && (grid[row - 1][col] & Direction::SOUTH) != Direction::BLOCKED) {
+        dir = dir | Direction::NORTH;
+    }
+
+    if (row < grid.size() - 1 && (grid[row + 1][col] & Direction::NORTH) != Direction::BLOCKED) {
+        dir = dir | Direction::SOUTH;
+    }
+
+    if (col > 0 && (grid[row][col - 1] & Direction::EAST) != Direction::BLOCKED) {
+        dir = dir | Direction::WEST;
+    }
+
+    if (col < grid[row].size() - 1 && (grid[row][col + 1] & Direction::WEST) != Direction::BLOCKED) {
+        dir = dir | Direction::EAST;
+    }
+
+    grid[row][col] = dir;
+}
+
+int get_steps_to_farthest_tile(const Grid &grid, const std::pair<int, int> &start) {
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    std::vector<std::vector<int>> steps(rows, std::vector<int>(cols, -1));
+    std::queue<std::pair<int, int>> q;
+
+    q.push(start);
+
+    steps[start.first][start.second] = 0;
+
+    int max_steps = 0;
+
+    while (!q.empty()) {
+       auto cur = q.front();
+       q.pop();
+
+        for (auto &dir: get_offsets(grid[cur.first][cur.second])) {
+            auto next = std::make_pair(cur.first + dir.first, cur.second + dir.second);
+
+            if (next.first < 0 || next.first >= rows || next.second < 0 || next.second >= cols) {
+                continue;
+            }
+
+            if (steps[next.first][next.second] != -1) {
+                continue;
+            }
+
+            auto next_steps = steps[cur.first][cur.second] + 1;
+            steps[next.first][next.second] = next_steps;
+
+            max_steps = std::max(max_steps, next_steps);
+
+            q.push(next);
+        }
+    }
+
+    return max_steps;
+}
+
+std::vector<std::pair<int, int>> get_offsets(Direction direction) {
+    std::vector<std::pair<int, int>> offsets;
+
+    if ((direction & Direction::NORTH) != Direction::BLOCKED) {
+        offsets.emplace_back(direction_offset.at(Direction::NORTH));
+    }
+
+    if ((direction & Direction::EAST) != Direction::BLOCKED) {
+        offsets.emplace_back(direction_offset.at(Direction::EAST));
+    }
+
+    if ((direction & Direction::SOUTH) != Direction::BLOCKED) {
+        offsets.emplace_back(direction_offset.at(Direction::SOUTH));
+    }
+
+    if ((direction & Direction::WEST) != Direction::BLOCKED) {
+        offsets.emplace_back(direction_offset.at(Direction::WEST));
+    }
+
+    return offsets;
 }
--- puzzle.h
+++ puzzle.h
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <queue>
 #include <ranges>
 #include <string>
 #include <sstream>
@@ -18,6 +19,47 @@
 #include <vector>
 
 #include "fmt/core.h"
+
+enum class Direction {
+    START = -1,
+    BLOCKED = 0,
+    NORTH = 1,
+    EAST = 2,
+    SOUTH = 4,
+    WEST = 8
+};
+
+// Overload | operator.
+constexpr Direction operator|(Direction a, Direction b) {
+    return static_cast<Direction>(static_cast<int>(a) | static_cast<int>(b));
+}
+
+// Overload & operator.
+constexpr Direction operator&(Direction a, Direction b) {
+    return static_cast<Direction>(static_cast<int>(a) & static_cast<int>(b));
+}
+
+const std::unordered_map<char, Direction> direction_map{
+        {'S', Direction::START},
+        {'.', Direction::BLOCKED},
+        {'|', Direction::NORTH | Direction::SOUTH},
+        {'-', Direction::WEST | Direction::EAST},
+        {'L', Direction::NORTH | Direction::EAST},
+        {'J', Direction::NORTH | Direction::WEST},
+        {'7', Direction::SOUTH | Direction::WEST},
+        {'F', Direction::SOUTH | Direction::EAST}
+};
+
+const std::unordered_map<Direction, std::pair<int, int>> direction_offset{
+        {Direction::NORTH, {-1, 0}},
+        {Direction::EAST,  {0,  1}},
+        {Direction::SOUTH, {1,  0}},
+        {Direction::WEST,  {0,  -1}}
+};
+
+using GridRow = std::vector<Direction>;
+using Grid = std::vector<GridRow>;
+
 
 int puzzle_sample_1(const std::string &base_file_path);
 
@@ -30,5 +72,11 @@ int puzzle_2(const std::string &base_file_path);
 int do_puzzle_1(std::ifstream &file);
 
 int do_puzzle_2(std::ifstream &file);
+
+void calculate_start_directions(Grid &grid, const std::pair<int, int> &start);
+
+int get_steps_to_farthest_tile(const Grid &grid, const std::pair<int, int> &start);
+
+std::vector<std::pair<int, int>> get_offsets(Direction direction);
 
 #endif //PUZZLE_H
